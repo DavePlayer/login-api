7ba8bb786deb85ca201b635a7ca3673e08587064
DavePlayer <daveplayer2001@gmail.com>
2020-05-28 18:29:52 +0200

    changed dotenv package and set temporary basic route

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log('initial')
+const express = require('express')
+require('dotenv').config()
+
+
+const app = express()
+console.log(process.env)
+
+app.listen(process.env.PORT)
+
+app.get('/', (req, res) => {
+    res.send('send nudes')
+})
+
